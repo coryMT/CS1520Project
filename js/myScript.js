6b6b840a93a8d97af147a98dec098bb376c83aec
Cory Trbojevic <cmt75@pitt.edu>
2016-10-13 14:32:30 -0400

    edited some styles

--- js/myScript.js
+++ js/myScript.js
@@ -5,3 +5,18 @@ $(window).scroll(function() {
     $('.nav').removeClass('sticky');
   }
 });
+
+function submitForm() {
+  if(document.getElementById('firstName').value == '') {
+    document.getElementById('error').innerHTML = 'You must enter your first name.';
+  } else if(document.getElementById('lastName').value == '') {
+    document.getElementById('error').innerHTML = 'You must enter your last name.';
+  } else if(document.getElementById('email').value == '') {
+    document.getElementById('error').innerHTML = 'You must enter your email address.';
+  } else if(document.getElementById('message').value == '') {
+    document.getElementById('error').innerHTML = 'Please send a short message about why you are contacting me!';
+  } else {
+    window.location.href = 'mailto:cmt75@pitt.edu';
+    submit();
+  }
+}
